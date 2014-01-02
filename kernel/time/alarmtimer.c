d68f2cbfcdcad631e72d79b826ea576c307e41c1
Kumar Gala <galak@codeaurora.org>
2014-01-02 14:15:29 -0600

    rtc: alarm: init power_on_alarm_lock mutex in alarmtimer_rtc_timer_init
    Moved mutex_init of power_on_alarm_lock into alarmtimer_rtc_timer_init
    so that if CONFIG_RTC_CLASS is not enabled, we dont try to initialize
    the mutex.  This fixes a build issue when !CONFIG_RTC_CLASS
    
    Change-Id: I270564b94fd54162aa177a2c9767655098662259
    Signed-off-by: Kumar Gala <galak@codeaurora.org>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -159,6 +159,8 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 
 static inline void alarmtimer_rtc_timer_init(void)
 {
+	mutex_init(&power_on_alarm_lock);
+
 	rtc_timer_init(&rtctimer, NULL, NULL);
 }
 
@@ -896,7 +898,6 @@ static int __init alarmtimer_init(void)
 		.nsleep		= alarm_timer_nsleep,
 	};
 
-	mutex_init(&power_on_alarm_lock);
 	alarmtimer_rtc_timer_init();
 
 	posix_timers_register_clock(CLOCK_REALTIME_ALARM, &alarm_clock);
